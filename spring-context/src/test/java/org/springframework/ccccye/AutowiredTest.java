819594e24d60939ea6d91d84fe28f7ebcff82310
ccccye <w0.0.3@163.com>
2021-03-26 00:13:09 +0800

    修改demo代码

--- spring-context/src/test/java/org/springframework/ccccye/AutowiredTest.java
+++ spring-context/src/test/java/org/springframework/ccccye/AutowiredTest.java
@@ -1,16 +1,25 @@
 package org.springframework.ccccye;
 
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.ccccye.IocService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class AutowiredTest {
 
 	@Test
 	public void autotest(){
-		ApplicationContext context = new ClassPathXmlApplicationContext("auto.xml");
-		IocService service = context.getBean("iocService", IocService.class);
-		System.out.println("count:" + service.getCount());
+		// Spring Framework 的方式
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("test.xml"));
+		Man man = factory.getBean("man", Man.class);
+		man.eat();
+
+		// Spring Boot 的方式
+//		ApplicationContext context = new ClassPathXmlApplicationContext("auto.xml");
+//		IocService service = context.getBean("iocService", IocService.class);
+//		System.out.println("count:" + service.getCount());
 	}
 }
